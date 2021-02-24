84d7bc374fc913fa6f571e7364f4720307adc52d
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-02-23 22:44:34 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Code your solution!")
+document.addEventListener("DOMContentLoaded", () => {
+  let submit = document.querySelector("button");
+  submit.addEventListener("click", (event) => {
+    event.preventDefault();
+    let list = document.querySelector("ul");
+    let text = String(document.querySelector("input").value);
+    addListItem(list, text);
+  });
+});
+
+const addListItem = (list, text) => {
+  let p = document.querySelector("p");
+  p.textContent = "";
+  if (text === "") {
+    p.textContent = "Error. Todo cannot be empty";
+  } else {
+    let li = document.createElement("li");
+    li.textContent += text;
+    list.appendChild(li);
+    li.addEventListener("click", (e)=>{
+        e.target.style.textDecoration = "line-through"
+    })
+  }
+};
